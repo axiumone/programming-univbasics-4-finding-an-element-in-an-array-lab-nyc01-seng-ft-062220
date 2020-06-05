f40700409d4744e564a80ef1114bd6f887783c8b
Dmitry Sychev <axiumone@gmail.com>
2020-06-05 22:03:26 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,10 +1,9 @@
-require pry
+
 
 def find_element_index(array, value_to_find)
   array.each_with_index do |ele, i|
     puts i
     # if i == value_to_find
-    #   binding.pry
     #   i
     # else
     #   nil
