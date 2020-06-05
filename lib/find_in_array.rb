dabb07eea4202f215806a9825833adc225b1aabc
Dmitry Sychev <axiumone@gmail.com>
2020-06-05 22:00:24 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,9 +1,8 @@
 def find_element_index(array, value_to_find)
-  array.each_with_index do { |ele, i|
+  array.each_with_index do |ele, i|
     if i == value_to_find
       i
     else
       nil
     end
-  }
 end
