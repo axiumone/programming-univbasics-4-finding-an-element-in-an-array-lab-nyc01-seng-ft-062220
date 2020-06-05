fd066e87502561cfc1e97f80c86d4660a21e61e6
Dmitry Sychev <axiumone@gmail.com>
2020-06-05 22:06:01 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,5 +1,10 @@
 
 
 def find_element_index(array, value_to_find)
-  array.select {|ele, i| i == value_to_find}
+  array.each_with_index do |ele, i|
+    if i == value_to_find
+      return value_to_find
+    end
+  end
+  return nil
 end
